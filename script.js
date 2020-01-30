caf7e7040ee1a598d44e2e293e131388b92e7407
di-shu <dmitry.shulyachenko@gmail.com>
2020-01-31 01:40:14 +0200

    mod slider and add links

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-  $('.main-slider').slick();
+  $('.main-slider').slick(
+  	{
+  		dots:true
+  	});
 
 
   document.title=document.getElementById("forTab").innerHTML;//заголовок вкладки по названию объявления
