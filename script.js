b2b27880c019d4c53e9f9071a12b9a0a7971931e
di-shu <dmitry.shulyachenko@gmail.com>
2020-01-31 04:45:44 +0200

    add modal slider

--- script.js
+++ script.js
@@ -5,3 +5,12 @@
 
 
   document.title=document.getElementById("forTab").innerHTML;//заголовок вкладки по названию объявления
+
+ let modal=document.getElementById("myModal");
+ let slider=document.getElementById("main-slider");
+
+ function fullscreen()
+ {
+ 	modal.style.display="block";
+ }
+
