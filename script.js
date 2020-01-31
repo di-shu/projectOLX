3cb8d35fc209c6bfb7e4b0be959ae1514d16b3c5
di-shu <dmitry.shulyachenko@gmail.com>
2020-01-31 23:51:12 +0200

    add new slider and fullcreen slider

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-  $('.main-slider').slick(
-  	{
-  		dots:true
-  	});
+ var swiper = new Swiper('.swiper-container', {
+ 	zoom:false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      },
+    });
 
 
-  document.title=document.getElementById("forTab").innerHTML;//заголовок вкладки по названию объявления
+ document.title=document.getElementById("forTab").innerHTML;//заголовок вкладки по названию объявления
 
- let modal=document.getElementById("myModal");
- let slider=document.getElementById("main-slider");
+ let photoComp=document.getElementById("qa-photo-component");
+ let qa_swiper=document.getElementsByClassName('swiper-slide');
+ let button_close=document.getElementById("btn-close-prev");
 
- function fullscreen()
- {
- 	modal.style.display="block";
- }
+
+ window.addEventListener('load',function(){
+
+	 for(let i=0;i<qa_swiper.length;i++)
+	 {
+	 	// if(qa_swiper[i])
+	 	qa_swiper[i].addEventListener('touchend',function(e){
+ 		photoComp.classList.add('is-preview-active');
+ 		button_close.style.display = "block";
+ 		
+ 	},false)
+	 }
+
+
+
+ 	// qa_swiper.addEventListener('touchend',function(e){
+ 	// 	photoComp.classList.add('is-preview-active');
+ 	// 	button_close.style.display = "block";
+ 	// },false)
+ 	button_close.addEventListener('touchend',function(e){
+ 		photoComp.classList.remove("is-preview-active");
+ 		button_close.style.display = 'none';
+ 	},false)
+
+ },false)
 
